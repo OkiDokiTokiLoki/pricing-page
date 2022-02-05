9c85faed2764759098cf1d658e12f76e14e03fae
tokiloki <codebadger1@gmail.com>
2022-02-05 09:45:05 +0200

    slider variables update

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const price = document.querySelector('#planAmount');
-const numInput = document.querySelector('#employeeCount');
 const rangeInput = document.querySelector('#rangeInput');
+const numInput = document.querySelector('#employeeCount');
+const price = document.querySelector('#priceDisplay');
 
 rangeInput.addEventListener('input', (e) => {
 
     numInput.value = e.target.value;
 
     if (e.target.value < 100){
-        price.textContent = '$10';
+        price.textContent = '$10.00';
     } else if (rangeInput.value < 500 ){
-        price.textContent = '$8';
+        price.textContent = '$8.00';
     } else if (rangeInput.value < 1000 ){
-        price.textContent = '$7';
+        price.textContent = '$7.00';
     } else if (rangeInput.value < 2000 ){
-        price.textContent = '$6';
+        price.textContent = '$6.00';
     } else if (rangeInput.value < 3000 ){
-        price.textContent = '$5';
+        price.textContent = '$5.00';
     } else{
-        price.textContent = '$4';
+        price.textContent = '$4.00';
     }
 })
 
@@ -26,17 +26,16 @@ numInput.addEventListener('input', (e) => {
     rangeInput.value = parseInt(numInput.value);
 
     if (e.target.value < 100){
-        price.textContent = '$10';
+        price.textContent = '$10.00';
     } else if (rangeInput.value < 500 ){
-        price.textContent = '$8';
+        price.textContent = '$8.00';
     } else if (rangeInput.value < 1000 ){
-        price.textContent = '$7';
+        price.textContent = '$7.00';
     } else if (rangeInput.value < 2000 ){
-        price.textContent = '$6';
+        price.textContent = '$6.00';
     } else if (rangeInput.value < 3000 ){
-        price.textContent = '$5';
+        price.textContent = '$5.00';
     } else{
-        price.textContent = '$4';
+        price.textContent = '$4.00';
     }
 })
-
