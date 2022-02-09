6acda98d24ac4ac0a6a1cf9d90c6aa584579eb8b
tokiloki <codebadger1@gmail.com>
2022-02-09 16:02:25 +0200

    refactor code

--- script.js
+++ script.js
@@ -3,28 +3,16 @@ const numInput = document.querySelector('#employeeCount');
 const price = document.querySelector('#priceDisplay');
 
 rangeInput.addEventListener('input', (e) => {
-
     numInput.value = e.target.value;
-
-    if (e.target.value < 100){
-        price.textContent = '$10.00';
-    } else if (rangeInput.value < 500 ){
-        price.textContent = '$8.00';
-    } else if (rangeInput.value < 1000 ){
-        price.textContent = '$7.00';
-    } else if (rangeInput.value < 2000 ){
-        price.textContent = '$6.00';
-    } else if (rangeInput.value < 3000 ){
-        price.textContent = '$5.00';
-    } else{
-        price.textContent = '$4.00';
-    }
+    priceCalc(e);
 })
 
 numInput.addEventListener('input', (e) => {
-    
     rangeInput.value = parseInt(numInput.value);
+    priceCalc(e);
+})
 
+function priceCalc(e){
     if (e.target.value < 100){
         price.textContent = '$10.00';
     } else if (rangeInput.value < 500 ){
@@ -38,4 +26,4 @@ numInput.addEventListener('input', (e) => {
     } else{
         price.textContent = '$4.00';
     }
-})
+}
